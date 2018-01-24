dd9d2be9511ed9fa273c9b1661b2ef562e5ed1d9
PJ Frias <peejfrias@gmail.com>
2018-01-24 13:09:02 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -32,8 +32,8 @@ class Dog
   end
 
   def save
-    binding.pry
     if self.id != nil
+      binding.pry
       self.update
     else
       sql = <<-SQL
@@ -43,6 +43,7 @@ class Dog
 
       DB[:conn].execute(sql,self.name, self.breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+      binding.pry
     end
   end
 
